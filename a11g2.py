0dd8ed0673ea3f2744a89dfc90a7919a2881b194
agustinpic72 <agustinpiccoli@gmail.com>
2022-06-10 23:39:32 -0300

    Resolvi issues 9-13-14-21

--- a11g2.py
+++ a11g2.py
@@ -1,1 +1,32 @@
 #Aqui se deberá insertar el código.
+
+#Issue nro 9
+def potencia(numeros):
+    return numeros[0]**numeros[1]
+
+#Issue nro 13
+def p3(numeros,c):
+    res=resta([numeros[0],numeros[1]])
+    res=producto([res,c])
+    return res
+
+#Issue nro 14
+def genrnd():
+    lista=[]
+    for i in range(50):
+        lista.append(random.randint(-256,+256))
+    return lista
+
+#Issue nro 21
+def calcula_varianza(lista):
+    #lista=[1500,1200,1700,1300,1800] --> 228.035
+    mean=calcula_media(lista)
+    varianza=[]
+    sumatoria=0
+    for i in lista:
+        varianza.append(potencia([resta([i,mean]),2]))
+    for i in varianza:
+        sumatoria=suma([sumatoria,i])
+    res=cociente([sumatoria,len(lista)])
+    res=radicacion([res,2])
+    return res
