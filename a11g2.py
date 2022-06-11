882489a696afbc16529ad5e4d6e963008b008837
ignacio <nro19975@gmail.com>
2022-06-11 18:25:45 -0300

    ejercicios y titulo

--- a11g2.py
+++ a11g2.py
@@ -1,9 +1,18 @@
 #Aqui se deberá insertar el código.
-
+#titulo
+print("arrancando el codigo del grupo a11g2")
+#Issue nro 6
+def producto(parametro1,parametro2):
+  return parametro1*parametro2
+  
 #Issue nro 9
 def potencia(numeros):
     return numeros[0]**numeros[1]
 
+  #Issue nro 11
+  def p1(parametro1,parametro2,parametro3):
+    return suma(producto(parametro1,parametro2),parametro3)
+    
 #Issue nro 13
 def p3(numeros,c):
     res=resta([numeros[0],numeros[1]])
@@ -16,6 +25,14 @@ def genrnd():
     for i in range(50):
         lista.append(random.randint(-256,+256))
     return lista
+#Issue nro 15
+  def suma_lista(lista):
+    acu=0
+    for i in range len(lista):
+        acu+=genrnd[i]
+     return acu
+
+suma_lista(genrnd())
 
 #Issue nro 21
 def calcula_varianza(lista):
