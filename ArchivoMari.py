22efdf0a86ec15f85bb662054e48c1cb6580f66e
Maribel Riesgo <maribelriesgo@gmail.com>
2022-06-13 21:53:09 -0300

    Issue 3

--- ArchivoMari.py
+++ ArchivoMari.py
@@ -1,9 +1,14 @@
 #crear una función llamada 'ing2s' que debe permitir el ingreso de 2 valores de tipo string
 
-def ing2s (string1,string2):
-		input
-		return string1 + string2
+def ing2s ():
+		string1=str (input ("Escriba primer string"));
+		string2=str (input ("Escriba segundo string"));
+		return string1 + string2;
 
 
 
 #Función de nombre calcula_mediana que debe retornar la mediana del array obtenido de la función genrnd genrnd: genera un array de 50 numeros aleatorios
+
+#Función que debe retornar el número mayor del array obtenido en genrnd
+
+#Función de nombre 'genrnd2' que debe devolver un array de '500.000.000.000.000.000' de numeros aleatorios (claramente no se debe ejecutar o generará un desbordamiento de memoria) ¡NO SE DEBE USAR NUMPY!
