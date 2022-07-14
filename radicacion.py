5d74dc93357390e2cf7a11e7d767e647102d37bb
agustinpic72 <agustinpiccoli@gmail.com>
2022-07-13 21:22:43 -0300

    Resuelvo ZeroDivissionError

--- radicacion.py
+++ radicacion.py
@@ -2,10 +2,16 @@
 # retornar la raiz del primero respecto al segundo
 
 def radicacion(a,b=1):
-    return round(a**(1/b),3)
+    try:
+        res=round(a**(1/b),3)
+    except ZeroDivisionError:
+        return 'No se puede dividir por cero'
+    else:
+        return res
 
-x=float(input('Ingrese el numero del cual quiere conocer su raiz: '))
-y=float(input('Ingrese el indice de radicacion: '))
 
-print(radicacion(x,y))
+#x=float(input('Ingrese el numero del cual quiere conocer su raiz: ')) ###Debug
+#y=float(input('Ingrese el indice de radicacion: '))  ###Debug
+#print(radicacion(x,y)) ###Debug
+ 
 
